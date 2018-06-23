441be139112ccfcb95a2e83da7ab9df9efd42b0f
Charlie Lin <hlin266@uwo.ca>
2018-06-23 11:03:53 -0400

    Add training data read-write

--- main.cpp
+++ main.cpp
@@ -1,12 +1,96 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <cstdlib>
+#include <cmath>
 #include <fstream>
 #include <sstream>
 
 using namespace std;
 
+////////////   CLASS TRAINING DATA   ////////////
+class TrainingData
+{
+public:
+    TrainingData(const string filename);
+    bool isEof(void) { return m_trainingDataFile.eof(); }
+    void getTopology(vector<unsigned> &topology);
+    // Returns the number of input values read from the file:
+    unsigned getNextInputs(vector<double> &inputVals);
+    unsigned getTargetOutputs(vector<double> &targetOutputVals);
+
+private:
+    ifstream m_trainingDataFile;
+};
+
+void TrainingData::getTopology(vector<unsigned> &topology)
+{
+    string line;
+    string label;
+
+    getline(m_trainingDataFile, line);
+    stringstream ss(line);
+    ss >> label;
+    if(this->isEof() || label.compare("topology:") != 0)
+    {
+        abort();
+    }
+
+    while(!ss.eof())
+    {
+        unsigned n;
+        ss >> n;
+        topology.push_back(n);
+    }
+    return;
+}
+
+TrainingData::TrainingData(const string filename)
+{
+    m_trainingDataFile.open(filename.c_str());
+}
+
+
+unsigned TrainingData::getNextInputs(vector<double> &inputVals)
+{
+    inputVals.clear();
+
+    string line;
+    getline(m_trainingDataFile, line);
+    stringstream ss(line);
+
+    string label;
+    ss >> label;
+    if (label.compare("in:") == 0) {
+        double oneValue;
+        while (ss >> oneValue) {
+            inputVals.push_back(oneValue);
+        }
+    }
+
+    return inputVals.size();
+}
+
+unsigned TrainingData::getTargetOutputs(vector<double> &targetOutputVals)
+{
+    targetOutputVals.clear();
+
+    string line;
+    getline(m_trainingDataFile, line);
+    stringstream ss(line);
+
+    string label;
+    ss>> label;
+    if (label.compare("out:") == 0) {
+        double oneValue;
+        while (ss >> oneValue) {
+            targetOutputVals.push_back(oneValue);
+        }
+    }
+
+    return targetOutputVals.size();
+}
+
+////////////   CLASS NEURON   ////////////
 struct Connection{
     double weight;
     double deltaWeight;
@@ -15,9 +99,6 @@ struct Connection{
 class Neuron;
 typedef vector<Neuron> Layer;
 
-
-
-////////////   CLASS NEURON   ////////////
 class Neuron{
 public:
     Neuron(unsigned numOutputs, unsigned myIndex);
@@ -39,8 +120,8 @@ private:
     static double activationFunction(double x);
     static double activationFunctionDerivative(double x);
     double m_gradient;
-};
 
+};
 
 double Neuron::eta = 0.15; //overall net learning rate
 double Neuron::alpha = 0.5; //the momentum, multiplier of last deltaWeight
@@ -66,7 +147,6 @@ void Neuron::updateInputWeights(Layer &prevLayer){
     }
 }
 
-
 double Neuron::sumDOW (const Layer &nextLayer) const{
     double sum = 0.0;
 
@@ -79,29 +159,25 @@ double Neuron::sumDOW (const Layer &nextLayer) const{
     return sum;
 }
 
-
 void Neuron::calcHiddenGradients(const Layer &nextLayer){
     double dow = sumDOW(nextLayer);
     m_gradient = dow * Neuron::activationFunctionDerivative(m_outputVal);
 }
-
 
 void Neuron::calcOutputGradients(double targetVals) {
     double delta = targetVals - m_outputVal;
     m_gradient = delta * Neuron::activationFunctionDerivative(m_outputVal);
 }
 
-
 double Neuron::activationFunction(double x) {
     // using the tanh
+
     return tanh(x);
 }
-
 
 double Neuron::activationFunctionDerivative(double x) {
     return 1.0 - x*x;
 }
-
 
 void Neuron::feedForward(const Layer &prevLayer) {
     double sum = 0.0;
@@ -109,7 +185,7 @@ void Neuron::feedForward(const Layer &prevLayer) {
     // sum the previous layer's outputs (which are inputs_
     // include bias node from previous layer
 
-    for (unsigned n=0; n<prevLayer.size(); ++n){
+    for (unsigned n=0; n < prevLayer.size(); ++n){
         sum += prevLayer[n].getOutputVal() * prevLayer[n].m_outputWeights[m_myIndex].weight;
     }
     m_outputVal = Neuron::activationFunction(sum);
@@ -127,10 +203,10 @@ Neuron::Neuron(unsigned numOutputs, unsigned myIndex){
 
 
 
-
 ////////////   CLASS NET   ////////////
 class Net {
 public:
+
     Net(const vector<unsigned> &topology);
     void feedForward (const vector<double> &inputVals);
     void backProp (const vector<double> &targetVals);
@@ -145,8 +221,6 @@ private:
 };
 
 double Net::m_recentAverageSmoothingFactor = 100.0; // Number of training samples to average over
-
-
 void Net::getResults(vector<double> &resultVals) const{
     resultVals.clear();
 
@@ -155,8 +229,7 @@ void Net::getResults(vector<double> &resultVals) const{
     }
 }
 
-
-void Net::backProp (const vector<double> &targetVals) {
+void Net::backProp (const std::vector<double> &targetVals) {
     // calculate the RMS error
     Layer &outputLayer = m_layers.back();
     m_error = 0.0;
@@ -190,12 +263,11 @@ void Net::backProp (const vector<double> &targetVals) {
         Layer &layer = m_layers[layerNum];
         Layer &prevLayer = m_layers[layerNum - 1];
 
-        for (unsigned n=0; n < layer.size() -1; ++n){
+        for (unsigned n=0; n < layer.size() - 1; ++n){
             layer[n].updateInputWeights(prevLayer);
         }
     }
 };
-
 
 void Net::feedForward (const vector<double> &inputVals) {
     // Check the num of inputVals euqal to neuronnum expect bias
@@ -216,12 +288,13 @@ void Net::feedForward (const vector<double> &inputVals) {
 }
 
 
+
 Net::Net(const vector<unsigned> &topology){
     unsigned numLayers = topology.size();
-    for (unsigned layerNum =0; layerNum < numLayers; ++ layerNum){
+    for (unsigned layerNum =0; layerNum < numLayers; ++layerNum){
         //this loop creates layers
         m_layers.push_back(Layer());
-        unsigned numOutputs = layerNum == topology.size() - 1 ? 0 : topology[layerNum + 1];
+        unsigned numOutputs = layerNum == topology.size() - 1 ? 0 :topology[layerNum + 1];
 
         for (unsigned neuronNum =0; neuronNum <= topology[layerNum]; ++neuronNum){
             //this loop creates neurons for each layer
@@ -233,32 +306,64 @@ Net::Net(const vector<unsigned> &topology){
     }
 }
 
+void showVectorVals(string label, vector<double> &v)
+{
+    cout << label << " ";
+    for(unsigned i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
 
 
 
 
 
 
-
-
-
+////////////   RUN HERE   ////////////
 int main()
 {
     ////////////   CONSTRUCTOR with # OF LAYERS   ////////////
+    TrainingData trainData("trainingData.txt");
+
+    //e.g., {3, 2, 1 }
     vector<unsigned> topology;
-    topology.push_back(3);
-    topology.push_back(2);
-    topology.push_back(1);
+    //topology.push_back(3);
+    //topology.push_back(2);
+    //topology.push_back(1);
+
+    trainData.getTopology(topology);
     Net myNet(topology);
 
+    vector<double> inputVals, targetVals, resultVals;
+    int trainingPass = 0;
+    while(!trainData.isEof())
+    {
+        ++trainingPass;
+        cout << endl << "Pass" << trainingPass;
 
-    vector<double> inputVals;
-    vector<double> targetVals;
-    vector<double> resultVals;
+        // Get new input data and feed it forward:
+        if(trainData.getNextInputs(inputVals) != topology[0])
+            break;
+        showVectorVals(": Inputs :", inputVals);
+        myNet.feedForward(inputVals);
 
-    ////////////   TRAINING   ////////////
-    myNet.feedForward(inputVals);
-    myNet.backProp(targetVals);
+        // Collect the net's actual results:
+        myNet.getResults(resultVals);
+        showVectorVals("Outputs:", resultVals);
 
-    myNet.getResults(resultVals);
+        // Train the net what the outputs should have been:
+        trainData.getTargetOutputs(targetVals);
+        showVectorVals("Targets:", targetVals);
+        assert(targetVals.size() == topology.back());
+
+        myNet.backProp(targetVals);
+
+        // Report how well the training is working, average over recnet
+        cout << "Net recent average error: "
+             << myNet.getRecentAverageError() << endl;
+    }
+
+    cout << endl << "Done" << endl;
 }
